529ef94cd72b7ad5b3e31631dd9208fbcd491315
Fergus Mullineux <frugras@gmail.com>
2016-02-23 14:46:18 +0000

    player page - not working yet

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -125,6 +125,87 @@ function main() {
    
 .controller('playersCtrl', function($scope) {
 
+	main();
+
+	function load() {
+    // $("#tabs").hide();
+    var id = $("#playerid").val();
+    // let's make sure it's a number
+    // if (/^[0-9]+$/.test(id)) {
+        var data = $.ajax({
+            url: "http://www.badsquash.co.uk/players.php?leaguetype=1&format=json",
+        }).done(display)
+          .fail(function() {
+              $("#msg").html("Error in AJAX request.");
+          });
+    // } else {
+    //     $("#msg").html("Error - id must be a number");
+    // }
+	}
+
+	function display(data) {
+	    var data = $.parseJSON(data);
+	    console.log(data);
+	    for (var i = 0; i < data.data.length; i++) {
+
+	    	// $("#players").append('<ion-item ng-repeat="item in items">' + data.data[i].level + '</ion-item>');
+	    	$("#poo").append("<ion-item>Hello!</ion-item>")
+	    	console.log("poo")
+	    }
+	    if (data.status == "good") {
+	        var level = data.data.summary.level;
+	        var name = data.data.summary.player;
+
+	        // $("#tab-main").html("Player: " + name);
+
+	        // var s = data.data.statistics;
+	        // $("#p_matches").html("Matches: " + s.matches +
+	        //                      " won " + s.matches_won +
+	        //                      " lost " + s.matches_lost);
+	        // // $("#matchesbar").progressbar({value: 100 * s.matches_win_ratio});
+	        // $("#p_games").html("Games: " + (s.games_won + s.games_lost) +
+	        //                      " won " + s.games_won +
+	        //                      " lost " + s.games_lost);
+	        // // $("#gamesbar").progressbar({value: 100 * s.games_win_ratio});
+	        // $("#p_points").html("Points: " + (s.points_won + s.points_lost) +
+	        //                      " won " + s.points_won +
+	        //                      " lost " + s.points_lost);
+	        // // $("#pointsbar").progressbar({value: 100 * s.points_win_ratio});
+
+	        // var matches = data.data.matches;
+	        // var matchdata = [];
+	        // for (var i = 0; i < matches.length; i++) {
+	        //     var t = readmatch(matches[i]);
+	        //     matchdata.push(t);
+	        // }
+	        // $("#dtable").dataTable({
+	        //     data: matchdata,
+	        //     columns: [
+	        //         {title: "Type"},
+	        //         {title: "Opponent"},
+	        //         {title: "Score"},
+	        //         {title: "Level before"},
+	        //         {title: "Level after"}
+	        //     ]
+	        // });
+
+	        // // $("#tabs").tabs();
+	        // $("#tabs").show();
+	        // $("#msg").html("Success, loaded data for player " + id);
+	    } else {
+	        $("#msg").html("Error - " + data.user_message);
+	    }
+	}
+
+	function main() {
+	    $("#tabs").hide();
+
+	    // $("#playerid").button();
+	    load();
+	    // $("#loadbutton").button().click(load);
+
+	}
+
 })
    
 .controller('settingsCtrl', function($scope) {
