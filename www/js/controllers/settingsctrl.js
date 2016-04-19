27c0dcee61c11989fd9b7c6de77782620ab7a1d7
artbegolli <ab14764@bristol.ac.uk>
2016-04-19 15:03:03 +0100

    commenting and documentation

--- www/js/controllers/settingsctrl.js
+++ www/js/controllers/settingsctrl.js
@@ -1,17 +1,22 @@
 starter.controller('settingsCtrl', function($scope, $state, $ionicHistory, $rootScope) {
+	
+	// hides ionic back button
 	$ionicHistory.nextViewOptions({
 		disableBack: true
 	});
+
 	// when cache clear button is pressed
 	$scope.clearCache = function() {
-		// from start of time clear cache
+		// from start of time clear entire cache
 		Cache.clean(315360000000);
+		// return confirmation message
 		$("#message").html("Cache emptied")
 		setTimeout(function() {
 			$("#message").empty()
 		}, 3000);
 	}
-	// when logging out, delete saved details
+
+	// when logging out delete saved details
 	$scope.logout = function() {
 		localStorage.removeItem("email");
 		localStorage.removeItem("password");
